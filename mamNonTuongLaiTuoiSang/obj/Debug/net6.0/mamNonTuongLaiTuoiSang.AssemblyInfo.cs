1c78607b26b3f7c76e76f6108c5c0cd80a52b589
kin1304 <nguyennha6a6kl@gmail.com>
2024-09-30 13:13:59 +0700

    #SCRUM-53 add view account folder

--- mamNonTuongLaiTuoiSang/obj/Debug/net6.0/mamNonTuongLaiTuoiSang.AssemblyInfo.cs
+++ mamNonTuongLaiTuoiSang/obj/Debug/net6.0/mamNonTuongLaiTuoiSang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mamNonTuongLaiTuoiSang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4ba6779fc6fa2a7067cd5824162982421e8533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c4206cedad9b25c39fb1ad20683c2eb541863e")]
 [assembly: System.Reflection.AssemblyProductAttribute("mamNonTuongLaiTuoiSang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mamNonTuongLaiTuoiSang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
