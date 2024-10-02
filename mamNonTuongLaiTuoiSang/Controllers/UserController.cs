b6f167e16fbe75e0f1d0ae90c64521c50392e953
VoVanPhung <phungtieuphu@gmail.com>
2024-10-02 20:15:12 +0700

    #Scrum-2 thêm controller user

---
+++ mamNonTuongLaiTuoiSang/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mamNonTuongLaiTuoiSang.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+
+            return RedirectToAction("Index", "User"); 
+        }
+        public IActionResult Index()
+        {
+            return View("User");
+        }
+    }
+}
