0b6d74ef991e65fe7596515c31b7322f6e8b58d2
VoVanPhung <phungtieuphu@gmail.com>
2024-10-14 20:24:08 +0700

    Đã làm lại trang xem thông tin phụ huynh,sửa trang chủ

--- mamNonTuongLaiTuoiSang/Controllers/AccountController.cs
+++ mamNonTuongLaiTuoiSang/Controllers/AccountController.cs
@@ -35,7 +35,7 @@ namespace mamNonTuongLaiTuoiSang.Controllers
                     session.SetString("UserEmail", parent.Email);
                     session.SetString("Password", parent.MatKhau);
 
-                    return RedirectToAction("User", "User");
+                    return RedirectToAction("PhuHuynh", "PhuHuynh", parent);
                 }
 
                 var user = db.NhanViens.SingleOrDefault(nv => nv.Email == email && nv.MatKhau == password);
