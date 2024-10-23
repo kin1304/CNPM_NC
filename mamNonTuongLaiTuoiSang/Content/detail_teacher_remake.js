1b05b7b5ebd3853b06d1e5267bf3be09693b2510
tunan7401 <tuann7401@gmail.com>
2024-10-23 12:29:28 +0700

    scrum 228

---
+++ mamNonTuongLaiTuoiSang/Content/detail_teacher_remake.js
@@ -0,0 +1,238 @@
+﻿const container_Masothue = document.getElementById('Masothue');
+const letters_Masothue = document.querySelectorAll('.letterMasothue');
+
+container_Masothue.addEventListener('mouseover', () => {
+    letters_Masothue.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Masothue.addEventListener('mouseout', () => {
+    letters_Masothue.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+
+const container_Trinhdochuyenmon = document.getElementById('Trinhdochuyenmon');
+const letters_Trinhdochuyenmon = document.querySelectorAll('.letterTrinhdochuyenmon');
+
+container_Trinhdochuyenmon.addEventListener('mouseover', () => {
+    letters_Trinhdochuyenmon.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Trinhdochuyenmon.addEventListener('mouseout', () => {
+    letters_Trinhdochuyenmon.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Sao = document.getElementById('Sao');
+const letters_Sao = document.querySelectorAll('.letterSao');
+
+container_Sao.addEventListener('mouseover', () => {
+    letters_Sao.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Sao.addEventListener('mouseout', () => {
+    letters_Sao.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Hovaten = document.getElementById('Hovaten');
+const letters_Hovaten = document.querySelectorAll('.letterHovaten');
+
+container_Hovaten.addEventListener('mouseover', () => {
+    letters_Hovaten.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Hovaten.addEventListener('mouseout', () => {
+    letters_Hovaten.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Namsinh = document.getElementById('Namsinh');
+const letters_Namsinh = document.querySelectorAll('.letterNamsinh');
+
+container_Namsinh.addEventListener('mouseover', () => {
+    letters_Namsinh.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Namsinh.addEventListener('mouseout', () => {
+    letters_Namsinh.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Gioitinh = document.getElementById('Gioitinh');
+const letters_Gioitinh = document.querySelectorAll('.letterGioitinh');
+
+container_Gioitinh.addEventListener('mouseover', () => {
+    letters_Gioitinh.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Gioitinh.addEventListener('mouseout', () => {
+    letters_Gioitinh.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Namlam = document.getElementById('Namlam');
+const letters_Namlam = document.querySelectorAll('.letterNamlam');
+
+container_Namlam.addEventListener('mouseover', () => {
+    letters_Namlam.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Namlam.addEventListener('mouseout', () => {
+    letters_Namlam.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Email = document.getElementById('Email');
+const letters_Email = document.querySelectorAll('.letterEmail');
+
+container_Email.addEventListener('mouseover', () => {
+    letters_Email.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Email.addEventListener('mouseout', () => {
+    letters_Email.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Diachi = document.getElementById('Diachi');
+const letters_Diachi = document.querySelectorAll('.letterDiachi');
+
+container_Diachi.addEventListener('mouseover', () => {
+    letters_Diachi.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Diachi.addEventListener('mouseout', () => {
+    letters_Diachi.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Sodienthoai = document.getElementById('Sodienthoai');
+const letters_Sodienthoai = document.querySelectorAll('.letterSodienthoai');
+
+container_Sodienthoai.addEventListener('mouseover', () => {
+    letters_Sodienthoai.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Sodienthoai.addEventListener('mouseout', () => {
+    letters_Sodienthoai.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Chucvu = document.getElementById('Chucvu');
+const letters_Chucvu = document.querySelectorAll('.letterChucvu');
+
+container_Chucvu.addEventListener('mouseover', () => {
+    letters_Chucvu.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Chucvu.addEventListener('mouseout', () => {
+    letters_Chucvu.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+
+
+
+const container_Vitri = document.getElementById('Vitri');
+const letters_Vitri = document.querySelectorAll('.letterVitri');
+
+container_Vitri.addEventListener('mouseover', () => {
+    letters_Vitri.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.add('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
+container_Vitri.addEventListener('mouseout', () => {
+    letters_Vitri.forEach((letter, index) => {
+        setTimeout(() => {
+            letter.classList.remove('jump');
+        }, index * 50); // Độ trễ 50ms giữa các chữ cái
+    });
+});
