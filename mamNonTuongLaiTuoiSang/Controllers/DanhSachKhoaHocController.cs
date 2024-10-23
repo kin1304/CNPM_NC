1bec4749b50576e817334eb38ad7c950eb702144
tunan7401 <tuann7401@gmail.com>
2024-10-24 01:41:19 +0700

    scrum 154,155 và chỉnh sửa html và javascript để phát nhạc cho scrum 224

---
+++ mamNonTuongLaiTuoiSang/Controllers/DanhSachKhoaHocController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mamNonTuongLaiTuoiSang.Controllers
+{
+    public class DanhSachKhoaHocController : Controller
+    {
+        public IActionResult danhsachkhoahoc()
+        {
+            return View();
+        }
+    }
+}
