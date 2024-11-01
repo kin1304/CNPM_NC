bd574d2387d2ac42f63520895c507363ba0d0001
kin1304 <nguyennha6a6kl@gmail.com>
2024-11-01 14:04:34 +0700

    Sửa lại cách trình bày trang main và gắn trang hoạt động ngoại khóa vào

--- mamNonTuongLaiTuoiSang/Controllers/MainController.cs
+++ mamNonTuongLaiTuoiSang/Controllers/MainController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using mamNonTuongLaiTuoiSang.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace mamNonTuongLaiTuoiSang.Controllers
 {
     public class MainController : Controller
     {
-        public ActionResult trangchu()
+        private const string baseURL = "https://localhost:5005/api/PhuHuynhs";
+        private HttpClient client = new HttpClient();
+        [HttpGet]
+        public ActionResult trangchu(string id)
         {
-            return View("trangchu");
+            ViewData["PhuHuynh"] = id;
+            TempData["PhuHuynh"] = id;
+            if(id == null)
+            {
+                return View();
+            }
+            PhuHuynh ph = new PhuHuynh();
+            HttpResponseMessage response = client.GetAsync(baseURL + "/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<PhuHuynh>(result);
+                if (data != null)
+                {
+                    ph = data;
+                }
+            }
+            return View(ph);
+        }
+
+        public ActionResult NgoaiKhoa(string id)
+        {
+            ViewData["PhuHuynh"] = id;
+            TempData["PhuHuynh"] = id;
+            return View();
         }
     }
 }
