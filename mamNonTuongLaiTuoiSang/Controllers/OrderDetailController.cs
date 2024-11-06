624492f00125951ed053e402215e9879abc78f96
tunan7401 <tuann7401@gmail.com>
2024-11-06 09:18:38 +0700

    trang hóa đơn nè

---
+++ mamNonTuongLaiTuoiSang/Controllers/OrderDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mamNonTuongLaiTuoiSang.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        public IActionResult OrderDetail()
+        {
+            return View();
+        }
+    }
+}
