570a6faa92db3a6507e0401cb4635ec7851f1f62
VoVanPhung <phungtieuphu@gmail.com>
2024-11-07 13:47:53 +0700

    Hien thi trang tin tuc va trang quan ly tiem chung

--- mamNonTuongLaiTuoiSang/Controllers/PhuhuynhController.cs
+++ mamNonTuongLaiTuoiSang/Controllers/PhuhuynhController.cs
@@ -92,5 +92,64 @@ namespace mamNonTuongLaiTuoiSang.Controllers
             }
             return View(kh);
         }
+
+        //
+        [HttpGet]
+        public IActionResult TiemChung(string id)
+        {
+            TempData["PhuHuynh"] = id;
+            ViewData["PhuHuynh"] = id;
+            PhuHuynh ph = new PhuHuynh();
+
+            // Lấy thông tin của phụ huynh từ API
+            HttpResponseMessage response = client.GetAsync(baseURL + "/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<PhuHuynh>(result);
+                if (data != null)
+                {
+                    ph = data;
+                }
+            }
+
+            // Lấy thông tin tiêm chủng của học sinh
+            var hocSinhId = ph.IdPh; 
+            List<TiemChung> tiemChungs = new List<TiemChung>();
+
+            HttpResponseMessage tiemChungResponse = client.GetAsync($"https://localhost:5005/api/TiemChung/{hocSinhId}").Result;
+            if (tiemChungResponse.IsSuccessStatusCode)
+            {
+                string tiemChungResult = tiemChungResponse.Content.ReadAsStringAsync().Result;
+                tiemChungs = JsonConvert.DeserializeObject<List<TiemChung>>(tiemChungResult);
+            }
+
+            ViewData["TiemChung"] = tiemChungs;  
+            return View(ph);  
+        }
+        [HttpPost]
+        public async Task<IActionResult> CapNhatTiemChung(int hocSinhId, TiemChung tiemChung)
+        {
+            if (ModelState.IsValid)
+            {
+                string data = JsonConvert.SerializeObject(tiemChung);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+                // API để cập nhật thông tin tiêm chủng
+                HttpResponseMessage response = await client.PostAsync($"https://localhost:5005/api/TiemChung/{hocSinhId}", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("TiemChung", new { id = hocSinhId });  
+                }
+                else
+                {
+                    string errorContent = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(errorContent); 
+                }
+            }
+            return View(tiemChung); // Trả về view nếu có lỗi
+        }
+
     }
 }
