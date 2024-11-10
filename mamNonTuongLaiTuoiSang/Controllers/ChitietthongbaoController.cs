1b4f28ee6c1a97907549c5f9a55c0dc43dae9a8a
tunan7401 <tuann7401@gmail.com>
2024-11-10 11:47:29 +0700

    scrum 171, scrumn 42

---
+++ mamNonTuongLaiTuoiSang/Controllers/ChitietthongbaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mamNonTuongLaiTuoiSang.Controllers
+{
+    public class ChitietthongbaoController : Controller
+    {
+        public IActionResult chitietthongbao()
+        {
+            return View();
+        }
+    }
+}
